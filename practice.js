0e9a94ef91b59fa94134589b7daab0a6f7f02525
faheemabk <130866594+faheemabk@users.noreply.github.com>
2025-11-21 10:29:03 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,22 @@
+//Write the program to greet a person given their first and last name
+//let firstName = ("faheem");
+//let lastName = ("manyar");
+//console.log('hello, good morning ' + firstName + lastName);
+
+//Write a program that greets a person based on their gender. (If else) 
+
+//let gender1 = "male";
+//let gender2 = "female";
+
+//3. Write a program that counts from 0 - 1000 and prints (for loop)
+//if (gender1 = "male") {
+//    console.log('hello male!')
+//} else {
+//    console.log('hello female')
+//}
+
+for(let i=1; i<=1000; i++){
+    console.log(i);
+}
+
+
