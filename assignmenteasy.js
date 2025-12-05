e7d7332b6e647af67c39e874e6bd7a97d47d8895
faheemabk <130866594+faheemabk@users.noreply.github.com>
2025-12-05 21:41:31 +0530

    Add files via upload

---
+++ assignmenteasy.js
@@ -0,0 +1,55 @@
+/* function isAnagram(str1, str2) {
+    let string1 = str1.split('').sort().join();
+    let string2 = str2.split('').sort().join();
+
+    if (string1 == string2) {
+        console.log('these are anagrams');
+    }
+    else {
+        console.log('these are not anagrams');
+    }
+}
+
+isAnagram('spar', 'rasp'); */
+
+
+    const transactions = [
+    {
+        id: 1,
+        timestamp: 1656076800000,
+        price: 10,
+        category: 'Food',
+        itemName: 'Pizza'
+    },
+    {
+        id: 2,
+        timestamp: 1656080400000,
+        price: 25,
+        category: 'Clothes',
+        itemName: 'T-shirt'
+    },
+    {
+        id: 3,
+        timestamp: 1656084000000,
+        price: 15,
+        category: 'Food',
+        itemName: 'Burger'
+    }
+];
+
+
+const categoryTotals = {}
+
+
+function calculateTotalSpentByCategory(transactions){
+ if(transactions.category = 'Food'){ 
+  let ab = transactions['category']['price']  = 10 + 15;
+  let cd = {};
+  categoryTotals.push({
+  cd.push('ab');
+  })
+ }
+}
+
+
+console.log(cd);
